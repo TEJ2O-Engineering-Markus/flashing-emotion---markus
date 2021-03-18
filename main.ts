0c5724d8f2d6ff9f6e31a25f0b88bfab0c7481c5
markus-janda <80055070+markus-janda@users.noreply.github.com>
2021-03-18 14:12:15 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.pause(100)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(100)
+    basic.showIcon(IconNames.Sad)
+    basic.pause(100)
+    basic.showIcon(IconNames.Surprised)
+})
